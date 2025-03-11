da7f12f31b7a69d3d0b1e674df42acee7ad68ca5
dinhthanhhai <dthk164@gmail.com>
2025-03-11 15:39:46 +0700

    Initial project setup with React, Vite, and TailwindCSS

---
+++ src/routes/index.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+
+function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRouter;
